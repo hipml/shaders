d99fab692d126adf0a01b9169cbb027eae9b02e5
Paul Lambert <paul.lambert@linux.com>
2024-05-24 14:53:40 -0500

    albedo and alpha

--- spatial-shaders/scripts/spatial_shaders.gd
+++ spatial-shaders/scripts/spatial_shaders.gd
@@ -1,7 +1,12 @@
 extends Node3D
 
+@onready var sphere_example: MeshInstance3D = $SphereExample
+
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
-
-
+	
+	if Input.is_action_just_pressed("random_color"):
+		var sphere_mat: ShaderMaterial = sphere_example.get_active_material(0)
+		sphere_mat.set_shader_parameter("sphere_color", Vector3(randf(), randf(), randf()))
+		
